71d7a993829420c274febcd016365444db3acb5f
Zac Lovoy <zwlovoy@gmail.com>
2014-11-15 09:10:32 +0000

    Working on stuff

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-    $('#text').css({
-        position:'absolute',
-        left: ($(window).width() - $('#text').outerWidth())/2,
-        top: ($(window).height() - $('#text').outerHeight())/2
-    });
+    jQuery("#text").fitText();
 });
