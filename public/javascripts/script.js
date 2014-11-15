8abc0d0051ba39266423ac12e890fbdcf2057b81
Zac Lovoy <zwlovoy@gmail.com>
2014-11-15 21:28:38 +0000

    Attempted to fix time

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
-var checkRate = 60000*5;
+var checkRate = 5000;
 
 $(document).ready(function() {
+  checkStatus();
   setInterval(function(){
       checkStatus();
     },checkRate);
@@ -14,10 +15,8 @@ function checkStatus() {
     				success: function(output) {
                 if (output.inAuction) {
                   $("#status").text("Desert Bus IS Currently Auctioning AWESOMENESS!!!");
-                  checkRate = 5000;
                 } else {
                   $("#status").text("Desert Bus IS NOT Currently Auctioning");
-                  checkRate = 60000*5;
                 }
               if (output.goingOnce) {
                 $("#going").text("GOING ONCE!");
