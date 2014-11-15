798398b4d04c4511bf45c3119ffa6cfc59b97be2
Zac Lovoy <zwlovoy@gmail.com>
2014-11-15 09:31:11 +0000

    Working on stuff

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,43 @@
 $(document).ready(function() {
+  setInterval(function(){
+      checkStatus();
+    },5000);
 });
+
+function checkStatus() {
+    $.ajax({
+    				type: 'GET',
+    				url: "/status",
+    				dataType: 'json',
+    				success: function(output) {
+                if (output.inAuction) {
+                  $("#status").text("Desert Bus IS Currently Auctioning");
+                } else {
+                  $("#status").text("Desert Bus IS NOT Currently Auctioning");
+                }
+              if (output.goingOnce) {
+                $("#going").text("GOING ONCE!");
+              } else if (output.goingTwice) {
+                $("#going").text("GOING TWICE!!");
+              } else if (output.sold) {
+                $("#going").text("SOLD!!!!!!!");
+              }
+              $("#price").text(output.price);
+              if (output.startTime !== null) {
+                $("#startTime").text(output.startTime);
+              } else {
+                $("#startTime").text("???");
+              }
+              if (output.endTime !== null) {
+                $("#endTime").text(output.endTime);
+              } else {
+                $("#endTime").text("???");
+              }
+              $("#bidder").text("High Bidder: " + output.highBidder);
+    				},
+    				async: true,
+    				error: function() {
+    					
+    				}
+				});
+    }
