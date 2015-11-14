d2f2dffc4a3305b683e08d515d513b4eb927977e
Zac Lovoy <zwlovoy@gmail.com>
2015-11-14 02:01:48 +0000

    Test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var status = require('./routes/status');
 
 var app = express();
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
